44936479b0845bbd575bbb39ccc64201e0f3cbf6
JamesBoggs <166553741+JamesBoggs@users.noreply.github.com>
2025-10-25 21:41:22 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',               // required for S3/CloudFront
+  images: { unoptimized: true },  // no image optimizer on S3
+  eslint: { ignoreDuringBuilds: true }
 };
-
 module.exports = nextConfig;
